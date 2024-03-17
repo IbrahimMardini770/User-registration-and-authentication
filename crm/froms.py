2a4d2837f19bc8ae24a45117baed19a9b4cdf3fc
Ibrahim Mardini <159878097+IbrahimMardini770@users.noreply.github.com>
2024-03-17 13:30:38 +0300

    first

---
+++ crm/froms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
+
+from django.contrib.auth.models import User
+
+from django import forms
+
+from django.forms.widgets import PasswordInput , TextInput
+
+# Register user (Model Form)
+class CreateUserForm(UserCreationForm):
+    
+    class Meta:
+        
+        model=User
+        fields = ['username' , 'email' , 'password1' , 'password2']
+        
+        
+        
+        
+# Authinticate a user (Model Form)
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget = PasswordInput())
